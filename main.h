f259046fadfdd7b9205bea9ef27c7eda67bd2581
La Minh Duc <laminhduc200@gmail.com>
2023-11-05 14:59:23 +0700

    add maximum flow project

---
+++ main.h
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <limits.h>
+#include <queue>
+#include <string.h>
+#include <fstream>
+#include <ctime>
+#include <cstdlib>
+
+using namespace std;
+
+int fordFulkerson(int **graph, int s, int t);
+void setV();
+void create_data();
